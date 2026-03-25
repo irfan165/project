22fe32a22eedf4e7db9401fc5dcc533fc898b8f1
irfan165 <sharif65000@gmail.com>
2026-03-24 22:35:50 -0400

    setup

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import sklearn
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+file_path = "Dataset/diabetes_binary_5050split_health_indicators_BRFSS2015.csv"
+
 if __name__ == "__main__":
-    main()
+    pass
